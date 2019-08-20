2f04b2b700d6372e30d177748855aa4f3e83bb17
umekow <umekolashun@gmail.com>
2019-08-20 15:27:04 -0500

    required callbacks challenge

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -41,24 +41,68 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length); 
 }
+//test for getLength()
+console.log(getLength(items, amount => `I have ${amount} items`));
+
+
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]); 
 }
+//test for last()
+console.log(last(items, item => `${item} is the last item I have.`));
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  let result = x + y; 
+  return cb(result); 
 }
+//test for sumNums()
+console.log(sumNums(2, 3, result => `The sum of x and y is: ${result}`));
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  let product = x * y; 
+  return cb(product); 
 }
+//test for multiplyNums()
+console.log(multiplyNums(4, 3, product => `The product of x and y is: ${product}`)); 
+
+
+//callback for contains to perform multiple tests
+
+let cbFunction = item => `Does this list contain the item that you are looking for? ${item}`;
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  let ys= false; 
+  list.forEach(element => {
+    if (element === item) {
+      ys = true; 
+      return cb(ys);
+    }
+  });
+  return ys; 
 }
+
+
+
+//tests for contains()
+  //test1
+  console.log(contains('Gum', items, cbFunction));
+  //test2
+  console.log(contains("Hot cheetos", items, cbFunction));
+  //test3
+  console.log(contains('Lipstick', items, cbFunction)); 
+  //test4
+  console.log(contains('Notebook', items, cbFunction)); 
+
+//end of tests for contains()
 
 /* STRETCH PROBLEM */
 
